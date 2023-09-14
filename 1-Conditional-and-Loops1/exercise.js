3d7cc383cb6254b23c43f0bbd764bcce063ae7c6
Marta <marta.ferraris25@gmail.com>
2023-09-14 18:22:04 +0200

    exercise js

--- 1-Conditional-and-Loops1/exercise.js
+++ 1-Conditional-and-Loops1/exercise.js
@@ -1,10 +1,28 @@
-// ceo => 2200
-// manager => 1800
-// cto => 1800
-// developer => 1500
-// default => 1000
+//ceo => 2200
+//manager => 1800
+//cto => 1800
+//developer => 1500
+//default => 1000
+
 function calculateSalary(role) {
-  // ...
+  let salaryBase;
+  switch (role) {
+    case "ceo":
+      salaryBase = "2200€";
+      break;
+    case "manager":
+      salaryBase = "1800€";
+      break;
+    case "cto":
+      salaryBase = "1800€";
+      break;
+    case "developer":
+      salaryBase = "1500€";
+      break;
+    default:
+      salaryBase = "1000€";
+  }
+  return salaryBase;
 }
 
 const ceoSalary = calculateSalary('ceo');
