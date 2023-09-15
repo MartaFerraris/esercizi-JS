362e8792b1ed5ce910cc64b2b7f143430e6a3fdf
Marta <marta.ferraris25@gmail.com>
2023-09-15 11:52:36 +0200

    exercise

--- 5-Object-copying/exercise.js
+++ 5-Object-copying/exercise.js
@@ -4,9 +4,15 @@ const person1 = {
   age: 25,
 };
 
-const person2 = person1;
+
+const person2 = {
+  firstName: "Jonh",
+  lastName: "Doe",
+  age: 25,
+};
 
 // Modify the property `firstName` of the `person2` in "Simon".
+person2.firstName = "Simon";
 
 console.log(person1);
 console.log(person2);
