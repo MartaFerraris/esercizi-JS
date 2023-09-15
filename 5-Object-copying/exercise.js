05e8b8a5f758361d6d65eb2461779e20e16abe5b
Marta <marta.ferraris25@gmail.com>
2023-09-15 12:29:05 +0200

    exercise: complete

--- 5-Object-copying/exercise.js
+++ 5-Object-copying/exercise.js
@@ -12,3 +12,8 @@ person2.firstName = "Simon";
 
 console.log(person1);
 console.log(person2);
+
+//Perchè con la riassegnazione non copio l'oggetto in sè, ma solo il suo riferimento 
+//quindi il suo indirizzo di memoria. 
+//Modificando una proprietà di person2 in realtà modifico il valore della stessa 
+//proprietà di partenza di person1.
