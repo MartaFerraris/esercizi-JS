ef62bfae59541340af1a44bc9b6b6074287737f8
Marta <marta.ferraris25@gmail.com>
2023-09-22 19:55:44 +0200

    exercise: complete

--- 19-Getters-and-Setters/exercise.js
+++ 19-Getters-and-Setters/exercise.js
@@ -1,5 +1,33 @@
 class Person {
-  // ...
+  constructor(firstName, lastName, age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  get firstName(){
+    return this._firstName;
+  }
+  get lastName(){
+    return this._lastName;
+  }
+  get age(){
+    return this._age;
+  }
+
+  set firstName(newFirstName) {
+    this._firstName = newFirstName;
+  }
+  set lastName(newLastName) {
+    this._lastName = newLastName
+  }
+  set age(newAge) {
+    this._age = newAge;
+  }
+
+  get fullName(){
+    return `${this._firstName} ${this._lastName}`;
+  }
 }
 
 const person = new Person('Mario', 'Rossi', 25);
