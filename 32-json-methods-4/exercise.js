40ee442fa61a775cf7be17fc6c30603f6368215e
Marta <marta.ferraris25@gmail.com>
2023-09-29 12:09:56 +0200

    exercise

---
+++ 32-json-methods-4/exercise.js
@@ -0,0 +1,16 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25,
+};
+
+const filterPerson= {
+  id: person.id,
+  age: person.age,
+};
+
+// const json = JSON.stringify(person);
+const json = JSON.stringify(filterPerson);
+
+console.log(json); // Should return: { id: 1, age: 25 }
