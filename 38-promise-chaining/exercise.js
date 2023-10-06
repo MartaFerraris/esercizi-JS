d2ad5492cc63b97a26bc53d439d5e8dedb4fc1c9
Marta <marta.ferraris25@gmail.com>
2023-10-06 17:50:22 +0200

    exercise completed

---
+++ 38-promise-chaining/exercise.js
@@ -0,0 +1,34 @@
+const firstPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const isLogged = true
+        if (isLogged) {
+            const randomNum = Math.random();
+            resolve(randomNum);
+        } else {
+            reject("Error");
+        }
+    }, 1000);
+});
+
+const secondPromise = (numberValue)=> {
+    return new Promise((resolve, reject)=> {
+        setTimeout(()=> {
+            if (numberValue > 0.5) {
+                resolve({name: "John", age: 24});
+            } else {
+                reject("NaN");
+            }
+        }, 1000);
+    });
+};
+
+firstPromise
+    .then((randomNum)=> {
+        return secondPromise(randomNum);
+    })
+    .then((finalData)=> {
+        console.log(finalData);
+    })
+    .catch((error)=> {
+        console.log(error.message);
+    });
